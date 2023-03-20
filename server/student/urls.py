5040e26716c7f47d6457c043864551348c569d1b
PoonKaYuen <jacky97260311@gmail.com>
2023-03-20 16:08:50 +0800

    add cv templates

--- server/student/urls.py
+++ server/student/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('cv_ui', views.student_CV_UI, name='cv_ui'),
+    path('cv_ui1', views.student_CV_UI1, name='cv_ui1'),
+    path('cv_ui2', views.student_CV_UI2, name='cv_ui2'),
 ]
--- server/student/views.py
+++ server/student/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def student_CV_UI(request):
     return render(request, 'student/cv.html', {'nav':'student'})
+
+def student_CV_UI1(request):
+    return render(request, 'student/cv-version-1.html', {'nav':'student'})
+
+def student_CV_UI2(request):
+    return render(request, 'student/cv-version-2.html', {'nav':'student'})
