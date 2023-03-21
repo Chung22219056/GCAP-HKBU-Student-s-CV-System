4cfb212e6caefa624f992eb1450bb3d341ced71b
Chung <22219056@life.hkbu.edu.hk>
2023-03-21 01:40:17 -0700

    created student dashboard page

--- server/student/urls.py
+++ server/student/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('dashboard', views.dashboard, name='dashboard'),
     path('cv_ui', views.student_CV_UI, name='cv_ui'),
     path('cv_ui1', views.student_CV_UI1, name='cv_ui1'),
     path('cv_ui2', views.student_CV_UI2, name='cv_ui2'),
--- server/student/views.py
+++ server/student/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+cv_list = [
+    {"thumbnail":"https://cdn-images.zety.com/images/zety/landings/templates/cascade@1x.png","title":"Cascade"},
+    {"thumbnail":"https://cdn-images.zety.com/images/zety/landings/templates/crisp@1x.png","title":"Crisp"},
+    {"thumbnail":"https://cdn-images.zety.com/images/zety/landings/templates/enfold@1x.png","title":"Enfold"},
+    
+]
+
+def dashboard(request):
+    return render(request, 'student/dashboard.html', {'nav':'student','cv_list':cv_list})
+
 def student_CV_UI(request):
     return render(request, 'student/cv.html', {'nav':'student'})
 
