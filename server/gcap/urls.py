07f0392839ceafe4cc97a7a932586276916a9f99
Chung2212 <123064890+Chung22219056@users.noreply.github.com>
2023-08-01 12:48:36 +0800

    Restructure files and directories

--- server/gcap/urls.py
+++ server/gcap/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
+    path('login/', include('login.urls')),
     path("manager/", include('manager.urls')),
     path("student/", include('student.urls')),
 ]
