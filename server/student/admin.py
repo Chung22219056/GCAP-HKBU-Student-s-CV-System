e5a830fbcd7d816ad1d1eb118b96495939c37db5
snow1032 <jacksonpoon1032@gmail.com>
2023-08-02 22:45:24 +0800

    fix database bug

--- server/student/admin.py
+++ server/student/admin.py
@@ -5,6 +5,7 @@ from . import models
 # admin.site.register(models.Account)
 admin.site.register(models.Student)
 admin.site.register(models.CvInfoBase)
+admin.site.register(models.Cv)
 # admin.site.register(models.CVLanguage)
 # admin.site.register(models.CVSkill)
 admin.site.register(models.Education)
